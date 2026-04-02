9bfca9aeaea173bce1d8e4438ee8fabcb6287cb6
Mitsuru Haga <mitsuru.haga@gmail.com>
2026-04-02 17:46:48 +0900

    chore: common に schemas, models を定義

---
+++ packages/common/src/schema/index.ts
@@ -0,0 +1,7 @@
+export * from "./models/note";
+export * from "./models/tag";
+export * from "./models/user";
+export * from "./schemas/auth";
+export * from "./schemas/note";
+export * from "./schemas/tag";
+export * from "./z";
---
+++ packages/common/src/schema/schemas/note.ts
@@ -0,0 +1,66 @@
+import { NOTE_SCOPE, NOTE_SCOPES } from "database/constants";
+import { NoteSchema } from "../models/note";
+import { z } from "../z";
+
+export { NOTE_SCOPE, NOTE_SCOPES };
+export type NoteScope = (typeof NOTE_SCOPES)[number];
+
+/**
+ * ノート一覧取得のレスポンススキーマ
+ */
+export const NoteListResponseSchema = z
+  .array(NoteSchema)
+  .openapi("NoteListResponse");
+
+/**
+ * ノート作成リクエストのスキーマ
+ */
+export const NoteCreateRequestSchema = z
+  .object({
+    content: z.string().openapi({ example: "New note content" }),
+    tags: z
+      .array(z.string())
+      .optional()
+      .openapi({ example: ["Work", "Personal"] }),
+    isPermanent: z.boolean().default(false).openapi({ example: false }),
+  })
+  .openapi("NoteCreateRequest");
+
+/**
+ * ノート更新リクエストのスキーマ
+ */
+export const NoteUpdateRequestSchema = z
+  .object({
+    content: z.string().optional().openapi({ example: "Updated note content" }),
+    tags: z
+      .array(z.string())
+      .optional()
+      .openapi({ example: ["Work"] }),
+    isPermanent: z.boolean().optional().openapi({ example: false }),
+    deletedAt: z
+      .string()
+      .datetime()
+      .nullable()
+      .optional()
+      .openapi({ example: null }),
+  })
+  .openapi("NoteUpdateRequest");
+
+/**
+ * ノート一覧取得のクバリデーションスキーマ
+ */
+export const NoteQuerySchema = z
+  .object({
+    tag: z.string().optional().openapi({ example: "work" }),
+    scope: z
+      .enum(NOTE_SCOPES)
+      .optional()
+      .default(NOTE_SCOPE.ALL)
+      .openapi({ example: "all" }),
+  })
+  .openapi("NoteQuery");
+
+export type NoteCreateRequest = z.infer<typeof NoteCreateRequestSchema>;
+export type NoteUpdateRequest = z.infer<typeof NoteUpdateRequestSchema>;
+export type NoteListResponse = z.infer<typeof NoteListResponseSchema>;
+export type NoteQuery = z.infer<typeof NoteQuerySchema>;
