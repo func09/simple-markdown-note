e8ffc69b1b5cf5d09163bfd2fbe358950ee3bc79
Mitsuru Haga <mitsuru.haga@gmail.com>
2026-04-05 14:31:09 +0900

    refactor(mobile): inline useNoteResource into useNoteScreen

--- apps/mobile/src/features/notes/hooks/index.ts
+++ apps/mobile/src/features/notes/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from "./useNoteDomain";
 export * from "./useNoteEffect";
 export * from "./useNoteLogic";
-export * from "./useNoteResource";
 export * from "./useNoteScreen";
 export * from "./useNoteState";
